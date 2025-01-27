e61ce88871aff8d9dc4e60b0fbf94980169452a0
John Teye Doku <86769906+johnteye@users.noreply.github.com>
2025-01-27 11:37:53 +0000

    Blog App

---
+++ app.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+
+const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+//method override allows you to use PUT and Delete requests
+const methodOverride = require('method-override');
+const connectDB = require('./server/config/db')
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo');
+const session = require('express-session');
+
+const app = express();
+const PORT = 5000 || process.env.PORT;
+
+//Database connection
+connectDB();
+
+//urlenconder and json lets you take in data from forms
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(methodOverride('_method'));
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }),
+    // cookie: { maxAge: 1000 * 60 * 60 * 24 }
+}));
+
+app.use(express.static('public'));
+//Templating engine
+app.use(expressLayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+
+app.use('/' ,require('./server/routes/main'));
+app.use('/' ,require('./server/routes/admin'));
+
+app.listen(PORT, () => {
+    console.log(`🚀 App listening on port ${PORT}`);
+})
---
+++ server/routes/admin.js
@@ -0,0 +1,165 @@
+const express = require("express");
+const router = express.Router();
+const Post = require("../models/Post");
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const adminLayout = "../views/layouts/admin";
+const authMiddleware = (req, res, next) => {
+  token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Unauthorized" });
+  }
+};
+
+router.get("/admin", async (req, res) => {
+  try {
+    const locals = {
+      title: "Admin",
+      description: "Admin Page",
+    };
+    res.render("admin/index", { locals, layout: adminLayout });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post("/admin", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+    res.cookie("token", token, { httpOnly: true });
+
+    res.redirect("/dashboard");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/dashboard", authMiddleware, async (req, res) => {
+  const data = await Post.find();
+  res.render("admin/dashboard", {data, layout: adminLayout});
+});
+
+
+router.get("/add-post", authMiddleware, async (req, res) => {
+  try {
+    
+    const data = await Post.find();
+    res.render("admin/add-post", {data, layout: adminLayout});
+  } catch (error) {
+    console.log(error);
+    
+  }
+});
+
+router.post("/add-post", authMiddleware, async (req, res) => {
+  try {
+    
+  try {
+    const newPost = await Post.create({
+      title: req.body.title,
+      body: req.body.body,
+    })
+
+    await Post.create(newPost);
+    res.redirect('/dashboard');
+    
+  } catch (error) {
+    console.log(error);
+    
+  }
+  } catch (error) {
+    console.log(error);
+    
+  }
+});
+
+router.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    try {
+      const user = await User.create({
+        username,
+        password: hashedPassword,
+      });
+
+      res.status(201).json({ message: "User created successfully", user });
+    } catch (error) {
+      if (error.code === 11000) {
+        res.status(400).json({ message: "Username already exists" });
+      }
+
+      res.status(500).json({ message: "Internal server error" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
+
+router.put("/edit-post/:id", authMiddleware, async (req, res) => {
+  try {
+    await Post.findByIdAndUpdate(req.params.id, {
+      title: req.body.title,
+      body: req.body.body,
+      updatedAt: Date.now(),
+    })
+
+    res.redirect('/post/' + req.params.id);
+ 
+  } catch (error) {
+    console.log(error);
+    
+  }
+});
+router.get("/edit-post/:id", authMiddleware, async (req, res) => {
+  try {
+    // const data = await Post.findById(req.params.id)
+    const data = await Post.findOne({ _id: req.params.id });
+    res.render('admin/edit-post', {data, layout: adminLayout});
+ 
+  } catch (error) {
+    console.log(error);
+    
+  }
+});
+
+router.delete('/delete-post/:id', authMiddleware, async (req, res) => {
+  try {
+    await Post.deleteOne({_id: req.params.id});
+    res.redirect('/dashboard');
+  } catch (error) {
+    console.log(error);
+  }
+
+}
+);
+
+router.get("/logout", (req, res) => {
+  res.clearCookie("token");
+  res.redirect("/");
+});
+
+module.exports = router;
