b8f817ab3ae4f3910c4cd77b0dc8f5d978c4b57b
John Teye Doku <86769906+johnteye@users.noreply.github.com>
2025-01-31 10:19:35 +0000

    Added Vercel.json

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const expressLayout = require('express-ejs-layouts');
+const express = require("express");
+const expressLayout = require("express-ejs-layouts");
 //method override allows you to use PUT and Delete requests
-const methodOverride = require('method-override');
-const connectDB = require('./server/config/db')
-const cookieParser = require('cookie-parser');
-const MongoStore = require('connect-mongo');
-const session = require('express-session');
-const cors = require('cors');
+const methodOverride = require("method-override");
+const connectDB = require("./server/config/db");
+const cookieParser = require("cookie-parser");
+const MongoStore = require("connect-mongo");
+const session = require("express-session");
+const cors = require("cors");
 
 const app = express();
 const PORT = 5000 || process.env.PORT;
@@ -20,28 +20,29 @@ connectDB();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 app.use(cors());
 
-app.use(session({
+app.use(
+  session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
     store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }),
     // cookie: { maxAge: 1000 * 60 * 60 * 24 }
-}));
+  })
+);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 //Templating engine
 app.use(expressLayout);
-app.set('layout', './layouts/main');
-app.set('view engine', 'ejs');
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
 
-
-app.use('/' ,require('./server/routes/main'));
-app.use('/' ,require('./server/routes/admin'));
-app.use('/api', require('./server/routes/api'));
+app.use("/", require("./server/routes/main"));
+app.use("/", require("./server/routes/admin"));
+app.use("/api", require("./server/routes/api"));
 
 app.listen(PORT, () => {
-    console.log(`🚀 App listening on port ${PORT}`);
-})
+  console.log(`🚀 App listening on port ${PORT}`);
+});
